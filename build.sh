95749d00b2bff0f770552b522483bb571c587a25
Afonso Caboz <afonso.caboz@gmail.com>
2025-07-28 11:10:32 +0100

    fix: build

--- build.sh
+++ build.sh
@@ -16,13 +16,13 @@ fi
 
 # Replace Firebase config placeholders in index.html
 if [ -f web/index.html ]; then
-  sed -i "s|\${FIREBASE_API_KEY}|\${FIREBASE_API_KEY}|g" web/index.html
-  sed -i "s|\${FIREBASE_AUTH_DOMAIN}|\${FIREBASE_AUTH_DOMAIN}|g" web/index.html
-  sed -i "s|\${FIREBASE_PROJECT_ID}|\${FIREBASE_PROJECT_ID}|g" web/index.html
-  sed -i "s|\${FIREBASE_STORAGE_BUCKET}|\${FIREBASE_STORAGE_BUCKET}|g" web/index.html
-  sed -i "s|\${FIREBASE_MESSAGING_SENDER_ID}|\${FIREBASE_MESSAGING_SENDER_ID}|g" web/index.html
-  sed -i "s|\${FIREBASE_APP_ID}|\${FIREBASE_APP_ID}|g" web/index.html
-  sed -i "s|\${FIREBASE_MEASUREMENT_ID}|\${FIREBASE_MEASUREMENT_ID}|g" web/index.html
+  sed -i "s|${FIREBASE_API_KEY}|${FIREBASE_API_KEY}|g" web/index.html
+  sed -i "s|${FIREBASE_AUTH_DOMAIN}|${FIREBASE_AUTH_DOMAIN}|g" web/index.html
+  sed -i "s|${FIREBASE_PROJECT_ID}|${FIREBASE_PROJECT_ID}|g" web/index.html
+  sed -i "s|${FIREBASE_STORAGE_BUCKET}|${FIREBASE_STORAGE_BUCKET}|g" web/index.html
+  sed -i "s|${FIREBASE_MESSAGING_SENDER_ID}|${FIREBASE_MESSAGING_SENDER_ID}|g" web/index.html
+  sed -i "s|${FIREBASE_APP_ID}|${FIREBASE_APP_ID}|g" web/index.html
+  sed -i "s|${FIREBASE_MEASUREMENT_ID}|${FIREBASE_MEASUREMENT_ID}|g" web/index.html
 fi
 
 # Build the Flutter web app
