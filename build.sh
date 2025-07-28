8a3c4c0052b90c9fe4a9ca85d6c38924fc57f6a2
Afonso Caboz <afonso.caboz@gmail.com>
2025-07-28 12:02:02 +0100

    fix:build

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ if [ -f .env ]; then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-# Generate app_config.dart from template
+escaped_mapbox_downloads_token=$(printf '%s' "$MAPBOX_DOWNLOADS_TOKEN" | sed -e 's/[\/&]/\\&/g')
+
 if [ -f lib/src/core/config/app_config.template.dart ]; then
-  sed -e "s#{{SUPABASE_URL}}#${SUPABASE_URL}#g" \
-      -e "s#{{SUPABASE_ANON_KEY}}#${SUPABASE_ANON_KEY}#g" \
-      -e "s#{{MAPBOX_ACCESS_TOKEN}}#${MAPBOX_ACCESS_TOKEN}#g" \
-      -e "s#{{MAPBOX_DOWNLOADS_TOKEN}}#${MAPBOX_DOWNLOADS_TOKEN}#g" \
+  sed -e "s/{{SUPABASE_URL}}/$escaped_supabase_url/g" \
+      -e "s/{{SUPABASE_ANON_KEY}}/$escaped_supabase_anon_key/g" \
+      -e "s/{{MAPBOX_ACCESS_TOKEN}}/$escaped_mapbox_access_token/g" \
+      -e "s/{{MAPBOX_DOWNLOADS_TOKEN}}/$escaped_mapbox_downloads_token/g" \
       lib/src/core/config/app_config.template.dart > lib/src/core/config/app_config.dart
 fi
 
