4a611e54761451c8d8aeb1bddfa7a783f5081bbd
Afonso Caboz <afonso.caboz@gmail.com>
2025-07-28 11:15:50 +0100

    fix:build

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ fi
 
 # Generate app_config.dart from template
 if [ -f lib/src/core/config/app_config.template.dart ]; then
-  sed -e "s|{{SUPABASE_URL}}|${SUPABASE_URL}|g" \
-      -e "s|{{SUPABASE_ANON_KEY}}|${SUPABASE_ANON_KEY}|g" \
-      -e "s|{{MAPBOX_ACCESS_TOKEN}}|${MAPBOX_ACCESS_TOKEN}|g" \
-      -e "s|{{MAPBOX_DOWNLOADS_TOKEN}}|${MAPBOX_DOWNLOADS_TOKEN}|g" \
+  sed -e "s#{{SUPABASE_URL}}#${SUPABASE_URL}#g" \
+      -e "s#{{SUPABASE_ANON_KEY}}#${SUPABASE_ANON_KEY}#g" \
+      -e "s#{{MAPBOX_ACCESS_TOKEN}}#${MAPBOX_ACCESS_TOKEN}#g" \
+      -e "s#{{MAPBOX_DOWNLOADS_TOKEN}}#${MAPBOX_DOWNLOADS_TOKEN}#g" \
       lib/src/core/config/app_config.template.dart > lib/src/core/config/app_config.dart
 fi
 
